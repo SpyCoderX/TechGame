2abb6bc14e0ae1d42a37cd7fd7cafb825dd06c53
BuildTools <unconfigured@null.spigotmc.org>
2023-11-25 21:42:54 -0500

    Added a bit of a checklist to the main file

--- Main.py
+++ Main.py
@@ -9,3 +9,27 @@ app = QApplication(sys.argv)
 B = Base()
 B.show()
 app.exec()
+
+""" 
+Add more code to:
+
+Entity
+LivingEntity
+Item
+Tile
+BaseEnemy
+Player
+
+
+Create:
+
+Camera.py
+PauseUI.py
+OverlayUI.py
+StartMenuUI.py
+Mode.py (For the current mode, Start Menu, In Game, Pause Menu, Shop Menu)
+Layer.py (Current layer/level (Normal/parallel))
+LevelLoader.py (Level loader, loads from a built-in set of levels)
+Levels/Level-<number>.json (The built-in levels)
+Tiles/__init__.py (The level, so this NEEDS to be made)
+"""
